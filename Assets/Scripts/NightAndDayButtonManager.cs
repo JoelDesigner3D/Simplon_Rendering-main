edce1a2485319a29228104a3b02db01096c58fe0
Collet <>
2023-10-27 08:46:43 +0200

    Jour / Nuit OK

--- Assets/Scripts/NightAndDayButtonManager.cs
+++ Assets/Scripts/NightAndDayButtonManager.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
+/**
+Unity ne permet pas de modifier directement l'asset Lighting Settings pendant l'exécution,
+car il s'agit d'une ressource statique chargée lors de la phase d'initialisation.
+*/
+
 
 public class NightAndDayButtonManager : MonoBehaviour
 {
-    private bool sunRises = true;
+    private bool sunRises;
 
     [Tooltip("The Text component uses to display the day/night state")]
     [SerializeField] private Text field;
@@ -13,12 +20,40 @@ public class NightAndDayButtonManager : MonoBehaviour
     [SerializeField] private Material skyboxDay;
     [SerializeField] private Material skyboxNight;
 
-    [SerializeField] private Color ambientLightColor;
+    [SerializeField] private Color ambientLightColorDay;
+    [SerializeField] private Color ambientLightColorNight;
+
+   // [SerializeField] private LightingSettings LightingSettingsDay;
+   // [SerializeField] private LightingSettings LightingSettingsNight;
+
+    //[SerializeField] private RenderSettings renderSettingsDay;
+   // [SerializeField] private RenderSettings renderSettingsNight;
+
+   // [SerializeField] private LightmapData lightmapDataDay;
+   // [SerializeField] private LightmapData lightmapDataNight;
+
+    //[SerializeField] private LightmapData LightmapDataDay;
+    // [SerializeField] private LightmapData LightmapDataNight;
 
 
-    private void Awake()
+
+    private void Start()
     {
-        field.text = "Jour";
+
+        Scene currentScene = SceneManager.GetActiveScene();
+        DebugXR.Instance.Log("Current scene is: " + currentScene.name);
+
+        if (currentScene.name == "DemoRendu")
+        {
+            sunRises = true;
+            field.text = "Jour";
+        }
+        else if(currentScene.name == "DemoRenduNight")
+        {
+            sunRises = false;
+            field.text = "Nuit";
+        }
+        
     }
 
 
@@ -28,19 +63,49 @@ public class NightAndDayButtonManager : MonoBehaviour
         {
             sunRises = false;
             field.text = "Nuit";
-            RenderSettings.skybox = skyboxNight;
 
-            RenderSettings.ambientLight = ambientLightColor;
+            SceneManager.LoadScene(1);
+
+            //ChangeLightingSettings(LightingSettingsNight);
+
+            //RenderSettings.skybox = skyboxNight;
+            //RenderSettings.ambientLight = ambientLightColorNight;
+
 
         }
         else
         {
             sunRises = true;
             field.text = "Jour";
-            RenderSettings.skybox = skyboxDay;
+
+            SceneManager.LoadScene(0);
+
+            //ChangeLightingSettings(LightingSettingsDay);
+
+            //RenderSettings.skybox = skyboxDay;
+            //RenderSettings.ambientLight = ambientLightColorDay;
         }
 
-        DynamicGI.UpdateEnvironment();
+       // DynamicGI.UpdateEnvironment();
+    }
+
+    public void ChangeLightingSettings(LightingSettings lightingSettings)
+    {
+        // Vérifiez si les paramètres d'éclairage ne sont pas nuls
+        if (lightingSettings != null)
+        {
+
+            //RenderSettings.ambientLight = 
+
+            /**
+
+            RenderSettings.sun = lightingSettings.sun;
+            RenderSettings.ambientMode = lightingSettings.ambientMode;
+            RenderSettings.ambientSkyColor = lightingSettings.ambientSkyColor;
+            RenderSettings.ambientEquatorColor = lightingSettings.ambientEquatorColor;
+            RenderSettings.ambientGroundColor = lightingSettings.ambientGroundColor;
+            */
+        }
     }
 
 
